70242c7cd3a2ecdc8ce2df615a2b9c10195184e3
Leonardo <leo_mc_inter@hotmail.com>
2022-02-22 19:09:55 -0300

    Finalizando ambiente inicial de projeto e incluindo dependências finais

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(errorHandler);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
