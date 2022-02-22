5787dc2e538b06fa3bd37cf3477447ae1b79a58b
Leonardo <leo_mc_inter@hotmail.com>
2022-02-22 19:25:56 -0300

    Incluindo arquivos de autenticação, utilidades, e pastas de camadas

---
+++ services/authServices.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { API_SECRET } = process.env;
+
+const JWT_CONFIG = {
+  expiresIn: "5h",
+  algorithm: "HS256",
+};
+
+const generateToken = (data) => jwt.sign({ data }, API_SECRET, JWT_CONFIG);
+
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, API_SECRET);
+    const user = decoded.data;
+
+    return user;
+  } catch (error) {
+    console.log(`FALHA NA AUTENTICAÇÃO -> ${error.message}`);
+    return null;
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
