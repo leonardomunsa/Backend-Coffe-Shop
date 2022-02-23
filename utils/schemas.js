a8d604641a853e37d121fcad69db78eb2b0ce779
Leonardo <leo_mc_inter@hotmail.com>
2022-02-23 13:44:11 -0300

    Adicionado esquema de validação para produtos

--- utils/schemas.js
+++ utils/schemas.js
@@ -16,10 +16,24 @@ const userSchema = Joi.object({
 const ingredientSchema = Joi.object({
   name: Joi.string().required().min(1),
   measurement: Joi.string().required().min(1),
-  unitPrice: Joi.number().required(),
+  unitPrice: Joi.number().required().min(0.1),
+});
+
+// validação de produto, com chave de componente sendo um array de objetos de ingredientes com quantidadee
+const productSchema = Joi.object({
+  name: Joi.string().required().min(1),
+  image: Joi.string().min(1),
+  price: Joi.number().required().min(0.1),
+  components: Joi.array().items(
+    Joi.object().keys({
+      ingredient: Joi.string().required().min(1),
+      quantity: Joi.number().min(1).required(),
+    })
+  ),
 });
 
 module.exports = {
   userSchema,
   ingredientSchema,
+  productSchema,
 };
