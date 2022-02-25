12f8c266694830f7bbb9169cda4ce7c45b65f935
Leonardo <leo_mc_inter@hotmail.com>
2022-02-25 01:38:02 -0300

    Pegando variável PORT de variável ambiente

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.use(router);
 
 // middleware para tratamento de erros
 app.use(errorHandler);
-
-const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
